54314f52ad09dce6fc159ef840f7a8f17d017625
Deric J Medina Rivera <dericgamer@gmail.com>
2023-05-22 22:24:01 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+app.get("/",function(req,res){
+  res.render("radio" )
+})
+
+app.get("/radio",function(req,res){
+  res.render("radio")
+})
+
+app.get("/contact",function(req,res){
+  res.render("contact")
+})
+
+app.get("/about",function(req,res){
+  res.render("about")
+})
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
